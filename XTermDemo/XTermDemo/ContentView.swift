e57e0f6f6cff07293a6515b972c1b7d50dad0122
Sheng Luo <luosheng1986@gmail.com>
2022-08-30 08:53:26 +0800

    Add demo app

---
+++ XTermDemo/XTermDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  XTermDemo
+//
+//  Created by Luo Sheng on 2022/8/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XTermDemo/XTermDemo/XTermDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  XTermDemoApp.swift
+//  XTermDemo
+//
+//  Created by Luo Sheng on 2022/8/30.
+//
+
+import SwiftUI
+
+@main
+struct XTermDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
