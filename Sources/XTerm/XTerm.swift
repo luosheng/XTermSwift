2564effc9649f40bc3dace88df9ff02c879b6155
Sheng Luo <luosheng1986@gmail.com>
2022-09-01 15:33:13 +0800

    Setup script handlers

--- Sources/XTerm/XTerm.swift
+++ Sources/XTerm/XTerm.swift
@@ -1,9 +1,26 @@
 import WebKit
 
+protocol XTermViewDelegate {
+  func onData(_ data: String)
+}
+
+protocol XTermHandler: WKScriptMessageHandler {
+  var delegate: XTermViewDelegate? { get set }
+}
+
 @available(macOS 12.0, *)
 public class XTermView: NSView {
   
   private var webView: WKWebView!
+  var delegate: XTermViewDelegate? {
+    didSet {
+      for i in 0..<handlers.count {
+        handlers[i].delegate = delegate
+      }
+    }
+  }
+  var handlers: [XTermHandler] = []
+  var userContentController = WKUserContentController()
   
   public override init(frame frameRect: NSRect) {
     super.init(frame: .zero)
@@ -17,6 +34,7 @@ public class XTermView: NSView {
   
   private func setup() {
     let configuration = WKWebViewConfiguration()
+    configuration.userContentController = userContentController
     webView = WKWebView(frame: .zero, configuration: configuration)
     webView.autoresizingMask = [.width, .height]
     self.addSubview(webView)
@@ -26,6 +44,11 @@ public class XTermView: NSView {
     }
     let indexURL = resourceURL.appendingPathComponent("index.html")
     webView.loadFileRequest(URLRequest(url: indexURL), allowingReadAccessTo: resourceURL)
+  }
+  
+  private func addHandler(_ handler: XTermHandler, name: String) {
+    self.userContentController.add(handler, name: name)
+    self.handlers.append(handler)
   }
   
   public func write(_ data: String) {
