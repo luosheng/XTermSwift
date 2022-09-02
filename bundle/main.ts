ca9e9118840c3ca1360a27c40af9ca636b0ec27f
Sheng Luo <luosheng1986@gmail.com>
2022-09-02 23:35:09 +0800

    Move js code to a single directory

---
+++ bundle/main.ts
@@ -0,0 +1,13 @@
+import { Terminal } from 'xterm'
+import 'xterm/css/xterm.css'
+
+const main = () => {
+  const term = new Terminal()
+  term.open(document.getElementById('terminal')!)
+
+  term.onData(data => {
+    window.webkit.messageHandlers.xtermOnData.postMessage(data)
+  })
+}
+
+main()
