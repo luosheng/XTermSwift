bf1d934e61ea9244bde7c5a522a6fbbd286a2a6c
Sheng Luo <luosheng1986@gmail.com>
2022-09-06 21:16:24 +0800

    Add theme definition

---
+++ Sources/XTerm/Models/Theme.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by Luo Sheng on 2022/9/6.
+//
+
+import Foundation
+
+public typealias HTMLColor = String
+
+public struct Theme: Codable {
+  var background: HTMLColor?
+  var black: HTMLColor?
+  var blue: HTMLColor?
+  var brightBlack: HTMLColor?
+  var brightBlue: HTMLColor?
+  var brightCyan: HTMLColor?
+  var brightGreen: HTMLColor?
+  var brightMagenta: HTMLColor?
+  var brightRed: HTMLColor?
+  var brightWhite: HTMLColor?
+  var brightYellow: HTMLColor?
+  var cursor: HTMLColor?
+  var cursorAccent: HTMLColor?
+  var cyan: HTMLColor?
+  var foreground: HTMLColor?
+  var green: HTMLColor?
+  var magenta: HTMLColor?
+  var red: HTMLColor?
+  var selection: HTMLColor?
+  var white: HTMLColor?
+  var yellow: HTMLColor?
+}
