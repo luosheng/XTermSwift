a5b3469117525660959a3085ca337f577789ec59
Sheng Luo <luosheng1986@gmail.com>
2022-09-07 13:26:35 +0800

    Parse vscode color scheme

--- Sources/XTerm/Models/Theme.swift
+++ Sources/XTerm/Models/Theme.swift
@@ -33,6 +33,8 @@ public struct Theme: Codable {
   public var yellow: HTMLColor?
 }
 
+// MARK: - Plain JSON
+
 extension Theme {
   
   public static func from(jsonString: String) -> Theme {
@@ -48,4 +50,42 @@ extension Theme {
     let encoder = JSONEncoder()
     return (try? JSONSerialization.jsonObject(with: encoder.encode(self))) as? [String : String] ?? [:]
   }
+}
+
+// MARK: - VSCode style https://github.com/mbadolato/iTerm2-Color-Schemes/tree/master/vscode
+
+extension Theme {
+  
+  public static func fromVSCode(jsonString: String) -> Theme {
+    var theme = Theme()
+    guard let data = jsonString.data(using: .utf8),
+          let object = try? JSONSerialization.jsonObject(with: data) as? [String : [String : String]],
+          let colors = object["workbench.colorCustomizations"] else {
+      return theme
+    }
+    
+    theme.foreground = colors["terminal.foreground"]
+    theme.background = colors["terminal.background"]
+    theme.black = colors["terminal.ansiBlack"]
+    theme.blue = colors["terminal.ansiBlue"]
+    theme.cyan = colors["terminal.ansiCyan"]
+    theme.green = colors["terminal.ansiGreen"]
+    theme.magenta = colors["terminal.ansiMagenta"]
+    theme.red = colors["terminal.ansiRed"]
+    theme.white = colors["terminal.ansiWhite"]
+    theme.yellow = colors["terminal.ansiYellow"]
+    theme.brightBlack = colors["terminal.ansiBlack"]
+    theme.brightBlue = colors["terminal.ansiBrightBlue"]
+    theme.brightCyan = colors["terminal.ansiBrightCyan"]
+    theme.brightGreen = colors["terminal.ansiBrightGreen"]
+    theme.brightMagenta = colors["terminal.ansiBrightMagenta"]
+    theme.brightRed = colors["terminal.ansiBrightRed"]
+    theme.brightWhite = colors["terminal.ansiBrightWhite"]
+    theme.brightYellow = colors["terminal.ansiBrightYellow"]
+    theme.selection = colors["terminal.selectionBackground"]
+    theme.cursor = colors["terminalCursor.foreground"]
+    
+    return theme
+  }
+  
 }
--- Tests/XTermTests/ThemeTestCase.swift
+++ Tests/XTermTests/ThemeTestCase.swift
@@ -10,6 +10,10 @@ import XTerm
 
 final class ThemeTestCase: XCTestCase {
   
+  let themeJSONString = """
+{"background": "#123456"}
+"""
+  
   override func setUpWithError() throws {
     // Put setup code here. This method is called before the invocation of each test method in the class.
   }
@@ -19,9 +23,6 @@ final class ThemeTestCase: XCTestCase {
   }
   
   func testJSON() throws {
-    let themeJSONString = """
-{"background": "#123456"}
-"""
     let theme = Theme.from(jsonString: themeJSONString)
     XCTAssertEqual(theme.background, "#123456")
     XCTAssertNil(theme.foreground)
@@ -31,4 +32,35 @@ final class ThemeTestCase: XCTestCase {
     XCTAssertEqual(dict["background"], "#123456")
   }
   
+  func testVSCode() throws {
+    let vscodeJson = """
+{
+    "workbench.colorCustomizations": {
+        "terminal.foreground": "#8f93a2",
+        "terminal.background": "#0f111a",
+        "terminal.ansiBlack": "#546e7a",
+        "terminal.ansiBlue": "#82aaff",
+        "terminal.ansiCyan": "#89ddff",
+        "terminal.ansiGreen": "#c3e88d",
+        "terminal.ansiMagenta": "#c792ea",
+        "terminal.ansiRed": "#ff5370",
+        "terminal.ansiWhite": "#ffffff",
+        "terminal.ansiYellow": "#ffcb6b",
+        "terminal.ansiBrightBlack": "#546e7a",
+        "terminal.ansiBrightBlue": "#82aaff",
+        "terminal.ansiBrightCyan": "#89ddff",
+        "terminal.ansiBrightGreen": "#c3e88d",
+        "terminal.ansiBrightMagenta": "#c792ea",
+        "terminal.ansiBrightRed": "#ff5370",
+        "terminal.ansiBrightWhite": "#ffffff",
+        "terminal.ansiBrightYellow": "#ffcb6b",
+        "terminal.selectionBackground": "#1f2233",
+        "terminalCursor.foreground": "#ffcc00"
+    }
+}
+"""
+    let theme = Theme.fromVSCode(jsonString: vscodeJson)
+    XCTAssertEqual(theme.white, "#ffffff")
+    XCTAssertEqual(theme.selection, "#1f2233")
+  }
 }
